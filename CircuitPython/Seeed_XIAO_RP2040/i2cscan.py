48ead3d6741e40a6fbd17dbc2fd5dee3c76ea418
Doug Gilliland <doug@douglasgilliland.com>
2022-06-12 11:54:15 -0400

    Create i2cscan.py

---
+++ CircuitPython/Seeed_XIAO_RP2040/i2cscan.py
@@ -0,0 +1,28 @@
+# i2cscan.py
+# SPDX-FileCopyrightText: 2017 Limor Fried for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython I2C Device Address Scan"""
+# If you run this and it seems to hang, try manually unlocking
+# your I2C bus from the REPL with
+#  >>> import board
+#  >>> board.I2C().unlock()
+
+import time
+import board
+
+# To use default I2C bus (most boards)
+i2c = board.I2C()
+
+while not i2c.try_lock():
+    pass
+
+try:
+    print(
+        "I2C addresses found:",
+        [hex(device_address) for device_address in i2c.scan()],
+    )
+
+finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
+    i2c.unlock()
