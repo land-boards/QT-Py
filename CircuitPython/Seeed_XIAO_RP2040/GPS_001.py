9bece2040bfc6a585e1c72a7300f9dc4cd8c8f11
Doug Gilliland <doug@douglasgilliland.com>
2022-06-24 15:30:36 -0400

    Update GPS_001.py

--- CircuitPython/Seeed_XIAO_RP2040/GPS_001.py
+++ CircuitPython/Seeed_XIAO_RP2040/GPS_001.py
@@ -9,6 +9,7 @@
 # https://learn.adafruit.com/circuitpython-essentials/circuitpython-uart-serial
 #
 # Docs
+# https://docs.circuitpython.org/projects/gps/en/latest/api.html#implementation-notes
 # https://docs.circuitpython.org/projects/gps/en/latest/
 # https://github.com/adafruit/Adafruit_CircuitPython_GPS
 # 
@@ -83,3 +84,51 @@ while True:
                 except KeyError:
                     print("- no info")
         print()
+
+
+
+# namespace
+# {
+#   struct GPS_TP5_MSG
+#   {
+#     uint8_t   header1;
+#     uint8_t   header2;
+#     uint8_t   message_class;
+#     uint8_t   message_id;
+#     uint16_t  length;
+#     uint8_t   timepulse_idx;
+#     uint8_t   version;
+#     uint16_t  reserved;
+#     int16_t   antenna_cable_delay;
+#     int16_t   rf_group_delay;
+#     uint32_t  frequency_unlocked;
+#     uint32_t  frequency_locked;
+#     uint32_t  duty_cycle_unlocked;
+#     uint32_t  duty_cycle_locked;
+#     uint32_t  user_delay;
+#     uint32_t  flags;
+#     uint16_t  checksum;
+#   } __attribute__((packed));
+# 
+#   const uint16_t GPS_TP5_ACTIVE           = 0x01;
+#   const uint16_t GPS_TP5_SYNC_TO_GNSS     = 0x02;
+#   const uint16_t GPS_TP5_LOCKED_OTHER_SET = 0x04;
+#   const uint16_t GPS_TP5_IS_FREQUENCY     = 0x08;
+#   const uint16_t GPS_TP5_IS_LENGTH        = 0x10;
+#   const uint16_t GPS_TP5_ALIGN_TO_TOW     = 0x20;
+#   const uint16_t GPS_TP5_POLARITY_RISING  = 0x40;
+# 
+#   const uint32_t GPS_TP5_DUTY_CYCLE_50    = 0x80000000;
+#   
+#   uint16_t calculateChecksum(uint8_t *p, size_t len)
+#   {
+#     uint8_t ck_a = 0;
+#     uint8_t ck_b = 0;
+#     while (len--)
+#     {
+#       ck_a = ck_a + *p++;
+#       ck_b = ck_b + ck_a;
+#     }
+#     return ck_a | (ck_b << 8);
+#   }
+# }
