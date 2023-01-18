9679ee00e5762456298c428d300536cbe198ea37
Doug Gilliland <doug@douglasgilliland.com>
2023-01-17 22:28:34 -0500

    updates

--- CircuitPython/Seeed_XIAO_RP2040/ER-USB-MIDI/MIDI_Serial_to_USB.py
+++ CircuitPython/Seeed_XIAO_RP2040/ER-USB-MIDI/MIDI_Serial_to_USB.py
@@ -1,5 +1,7 @@
+# MIDI_Serial_to_USB.py
 # SPDX-FileCopyrightText: 2022 Liz Clark for Adafruit Industries
 # SPDX-License-Identifier: MIT
+# https://docs.circuitpython.org/projects/midi/en/latest/api.html#adafruit_midi
 
 import board
 import busio
@@ -15,18 +17,20 @@ from adafruit_midi.program_change import ProgramChange
 #  uart setup
 uart = busio.UART(board.TX, board.RX, baudrate=31250)
 #  midi channel setup
-# midi_in_channel = 1
+midi_in_channel = 1
 midi_out_channel = 1
 #  midi setup
 #  UART is setup as the input
 #  USB is setup as the output
 midi = adafruit_midi.MIDI(
-    midi_in=usb_midi.ports[0],
-    midi_out=uart,
-    in_channel=((0,9)),
+    midi_in=uart,
+    midi_out=usb_midi.ports[1],
+    in_channel=(midi_in_channel - 1),
     out_channel=(midi_out_channel - 1),
-    debug=False,
+    debug=True    
 )
+
+print("Running")
 
 while True:
     #  receive MIDI message over USB
@@ -34,6 +38,14 @@ while True:
     #  if a message is received...
     if msg is not None:
         #  send that message over UART
-        midi.send(msg)
+#        midi.send(msg)
 #        print message to REPL for debugging
-        print(msg)
+        if isinstance(msg, ControlChange):
+            print("CC CTL=",end='')
+            print(msg.control,end='')
+            print(", val=",end='')
+            print(msg.value)
+        elif isinstance(msg, PitchBend):
+            print("Pitch Blend=",msg.pitch_bend)
+        else:
+            print(msg)
